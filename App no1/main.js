0b81760b641a44ecc3411a88d90deb38a6aee955
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-09-14 20:43:32 +0200

    Update

--- App no1/main.js
+++ App no1/main.js
@@ -1,4 +1,22 @@
+'use strict';
 let accountsData = document.querySelector('#accounts-data');
+let allLink = document.querySelectorAll('.nav-link');
+let addAcount = document.querySelector('#add-account');
+let acounts = document.querySelector('#accounts');
+let views = document.querySelectorAll('.view');
+
+for (let i = 0; i < allLink.length; i++) {
+  allLink[i].addEventListener('click' , showView)
+}
+function showView(e){
+   e.preventDefault();
+    for (let i = 0; i < views.length; i++) {
+    views[i].style.display = 'none';
+        
+    }
+    let id = `#${this.getAttribute("href")}`;
+    document.querySelector(id).style.display = 'block';
+} 
 
 createAccount();
 
