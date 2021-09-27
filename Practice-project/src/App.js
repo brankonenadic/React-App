9dabd85ebb3a26305c0272b21afe6a50a85754bd
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-09-27 15:42:42 +0200

    Update

--- Practice-project/src/App.js
+++ Practice-project/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import AddUser from './Components/User/AddUser';
+import UserList from './Components/User/UserList';
 
 
 function App() {
+  const [userslist, setUsersList] = useState([]);
+const addUserHandler = (uName, uAge) => {
+  setUsersList((prevUsersList) => {
+    let id = Math.floor(Math.random() * 100);
+    return [...prevUsersList, {key: id, name: uName, age: uAge}]
+  })
+}
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userslist}/>
     </div>
   );
 }
