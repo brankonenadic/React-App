47f2151fdb939c8d3f130c1dddbc41531d92f886
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-04 09:55:10 +0200

    Update

--- Http Request/src/App.js
+++ Http Request/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -8,74 +9,82 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fachMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://my-react-http-8a60d-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
       if (!response.ok) {
-        throw new Error('Something went wrong !!!');
+        throw new Error('Something went wrong!');
       }
+
       const data = await response.json();
-      const transforMovies = data.results.map(movieData => {
+      const loadedMovie = [];
+      for (const key in data) {
+        loadedMovie.push({
+          id: key,
+          title: data[key].title,
+          releaseDate: data[key].releaseDate,
+          openingText: data[key].openingText
+        });
+      } 
+
+    /*   const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
+          openingText: movieData.opening_crawl,
           releaseDate: movieData.release_date,
-          openingText: movieData.opening_crawl
-        }
-      });
-      setMovies(transforMovies);
-
+        };
+      }); */
+      setMovies(loadedMovie);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const addMovieHandler = async (movie) => {
+    const response = await fetch('https://my-react-http-8a60d-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
+  let content = <p>Found no movies.</p>;
 
-  /* const fachMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films/').then(response => {
-      return response.json();
-    }).then(data => {
-      const transforMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          releaseDate: movieData.release_date,
-          openingText: movieData.opening_crawl
-        }
-      });
-      setMovies(transforMovies);
-    })
-  }
-   */
-
-  let content = <p>No found movies !!!</p>;
   if (movies.length > 0) {
-    content =  <MoviesList movies={movies} />;
+    content = <MoviesList movies={movies} />;
   }
+
   if (error) {
     content = <p>{error}</p>;
   }
+
   if (isLoading) {
-    content = <p>Loading....</p>;
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fachMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-      {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
 
 export default App;
-/*   {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {movies.length === 0 && !error && <p>No found movies !!!</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading....</p>} */
