55d121fb3844f50fdcdf914891daaa1d1388e213
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-06 12:58:05 +0200

    Update

--- react forms/src/components/SimpleInput.js
+++ react forms/src/components/SimpleInput.js
@@ -1,27 +1,39 @@
-import {useRef, useState} from 'react'
+import { useRef, useState } from 'react'
 
 const SimpleInput = (props) => {
-    const [enteredInput, setEnteredInput] = useState();
+    const inputRef = useRef();
+    const [enteredInput, setEnteredInput] = useState('');
+    const [inputIsValid, setInputIsValid] = useState(true);
 
     const enteredInputHandler = (e) => {
         setEnteredInput(e.target.value);
+        setInputIsValid(true);
     }
-const formSubmitHandler = (e) => {
-    e.preventDefault();
-    console.log(enteredInput);
-    setEnteredInput('');
-}
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={enteredInputHandler} value={enteredInput} />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        if (enteredInput.trim() === '') {
+            setInputIsValid(false);
+            return;
+        }
+        setInputIsValid(true);
+        console.log(enteredInput);
+        console.log(inputRef.current.value);
+        setEnteredInput('');
+
+    }
+    const inputClass = inputIsValid ? 'form-control' : 'form-control invalid';
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className={inputClass}>
+                <label htmlFor='name'>Your Name</label>
+                <input ref={inputRef} type='text' id='name' onChange={enteredInputHandler} value={enteredInput} />
+            </div>
+            {!inputIsValid && <p className='error-text'>Enter Your name !!!</p>}
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
