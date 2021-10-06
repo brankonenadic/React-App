4fe2e8c8fccb0fab456ea9b9b881b5b8e11422e2
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-06 12:55:50 +0200

    Update

--- react forms/src/components/SimpleInput.js
+++ react forms/src/components/SimpleInput.js
@@ -1,9 +1,21 @@
+import {useRef, useState} from 'react'
+
 const SimpleInput = (props) => {
+    const [enteredInput, setEnteredInput] = useState();
+
+    const enteredInputHandler = (e) => {
+        setEnteredInput(e.target.value);
+    }
+const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredInput);
+    setEnteredInput('');
+}
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={enteredInputHandler} value={enteredInput} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
