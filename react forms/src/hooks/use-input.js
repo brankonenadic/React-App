215283f012dfb273e740f883db294ceb169b769b
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-08 22:12:04 +0200

    Update

---
+++ react forms/src/hooks/use-input.js
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+
+const useInput = () => {
+const [inputValue, setInputValue] = useState();
+
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default useInput
