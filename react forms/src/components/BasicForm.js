e21dd690e6fab48a87c6bad53ca706f95c4e5721
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-11 13:25:55 +0200

    Update

--- react forms/src/components/BasicForm.js
+++ react forms/src/components/BasicForm.js
@@ -1,26 +1,49 @@
 import useNewInput from '../hooks/use-newInput'
 const BasicForm = (props) => {
 
-const {value: nameValue, inputHandler: nameHandler, inputBlur: nameInputBlur } = useNewInput();
+  const { value: nameValue, inputHasEror: nameInpuHasError, isValid: nameIsValid, inputHandler: nameHandler, inputBlur: nameInputBlur, ressetInput: resetNameInput } = useNewInput(value => value.trim() !== '');
+  const { value: lastNameValue, inputHasEror: lastNameInpuHasError, isValid: lastNameIsValid, inputHandler: lastNameHandler, inputBlur: lastNameInputBlur, ressetInput: resetLastNameInput } = useNewInput(value => value.trim() !== '');
+
+  let formIsValid = false;
+  if (nameIsValid && lastNameIsValid) {
+    formIsValid = true;
+  }
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (nameInpuHasError && lastNameInpuHasError) {
+      formIsValid = false;
+    }
+
+    console.log(nameValue);
+    console.log(lastNameValue);
+
+    resetNameInput();
+    resetLastNameInput();
+  }
+  const nameInputClass = !nameInpuHasError ? 'form-control' : 'form-control invalid';
+  const lastNameInputClass = !lastNameInpuHasError ? 'form-control' : 'form-control invalid';
 
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={nameInputClass}>
           <label htmlFor='name'>First Name</label>
           <input type='text' id='name' onBlur={nameInputBlur} onChange={nameHandler} value={nameValue} />
+          {nameInpuHasError && <p className={['error-text']}>Enter Your Name</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+
+        <div className={lastNameInputClass}>
+          <label htmlFor='lastName'>Last Name</label>
+          <input type='text' id='lastName' onBlur={lastNameInputBlur} onChange={lastNameHandler} value={lastNameValue} />
+          {lastNameInpuHasError && <p className={['error-text']}>Enter Your Last name</p>}
         </div>
       </div>
       <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <label htmlFor='emaič'>E-Mail Address</label>
+        <input type='email' id='email' />
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
--- react forms/src/hooks/use-newInput.js
+++ react forms/src/hooks/use-newInput.js
@@ -1,24 +1,34 @@
 import { useState } from "react";
 
-const useNewInput = () => {
+const useNewInput = (validateValue) => {
 
-const [inputValue, setInputValue] = useState('');
-const [inputThached, setInputThached] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [inputThached, setInputThached] = useState(false);
 
-const inputHandler = (e) => {
-    setInputValue(e.target.value);
-    
-}
-const inputBlur = () => {
-    setInputThached(true);
-    console.log(inputValue);
-}
+    const valueIsValid = validateValue(inputValue);
+    const inputHasEror = !valueIsValid && inputThached;
 
+    const inputHandler = (e) => {
+        setInputValue(e.target.value);
+
+    }
+    const inputBlur = () => {
+
+        setInputThached(true);
+
+    }
+    const ressetInput = () => {
+        setInputValue('');
+        setInputThached(false);
+    }
 
     return {
         value: inputValue,
+        inputHasEror,
+        isValid: valueIsValid,
         inputHandler,
-        inputBlur
+        inputBlur,
+        ressetInput
     };
 }
 
