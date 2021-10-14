8e6a70d7ff9bc3c38f7c93ae0f5bfcb8e936cae8
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-14 20:02:26 +0200

    Update

---
+++ React-Redux/src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'redux';
+
+const counterReduser = (state = { counter: 0 }, action) => {
+    if (action.tyoe === 'increment') {
+        return {
+            counter: state.counter + 1
+        };
+    }
+    if (action.tyoe === 'decrement') {
+        return {
+            counter: state.counter - 1
+        };
+    }
+    return state;
+}
+
+const store = createStore(counterReduser);
+
+export default store;
