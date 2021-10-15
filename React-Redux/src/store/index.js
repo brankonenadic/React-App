b272e8a1c1eafa3232b5d965a18840b2c1a642fd
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-15 11:21:08 +0200

    Update

--- React-Redux/src/store/index.js
+++ React-Redux/src/store/index.js
@@ -1,20 +1,30 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const currentState = {counter: 0, showCounter: true};
+
+const counterReducer = (state = currentState, action) => {
     if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter
     };
   }
   if (action.type === 'incrementes') {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter
     };
   }
     if (action.type === 'decrement') {
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter
         };
+    }
+    if (action.type === 'toggle') {
+      return {
+        showCounter: !state.showCounter
+       };
     }
     return state;
 };
