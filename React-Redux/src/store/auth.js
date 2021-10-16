8e8a8a82f11b5bb00c99a7159d48fe1e139bf988
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-16 15:35:49 +0200

    Update

---
+++ React-Redux/src/store/auth.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialAuthState = {
+    isAuth: false
+  };
+  
+  const authSlice =  createSlice({
+    name: 'auth',
+    initialState: initialAuthState,
+    reducers: {
+      logIn(state) {
+        state.isAuth = true;
+      },
+      logOut(state) {
+        state.isAuth = false;
+      }
+    }
+  });
+  export const authActions = authSlice.actions;
+  export default authSlice.reducer;
--- React-Redux/src/store/index.js
+++ React-Redux/src/store/index.js
@@ -1,48 +1,12 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter';
+import authReducer from './auth';
 
-const initialCounterState = { counter: 0, showCounter: true };
 
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    }
-  }
-});
-const initialAuthState = {
-  isAuth: false
-};
-
-const authSlice =  createSlice({
-  name: 'auth',
-  initialState: initialAuthState,
-  reducers: {
-    logIn(state) {
-      state.isAuth = true;
-    },
-    logOut(state) {
-      state.isAuth = false;
-    }
-  }
-});
 
 const store = configureStore({
-  reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
+  reducer: {counter: counterReducer, auth: authReducer}
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
 
