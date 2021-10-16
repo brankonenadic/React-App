0f23b7dbd3fb56decc375905c834485788810abd
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-16 12:06:53 +0200

    Ušdate

--- React-Redux/src/store/index.js
+++ React-Redux/src/store/index.js
@@ -17,7 +17,7 @@ createSlice({
       state.counter = state.counter + action.amount;
     },
     toggle(state) {
-      stete.counter = !state.counter;
+      state.counter = !state.counter;
     }
   }
 });
