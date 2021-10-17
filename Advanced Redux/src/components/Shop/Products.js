4f7da8f125aef6dad6bb400e0e7bc091464fad56
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-17 14:37:28 +0200

    Update

--- Advanced Redux/src/components/Shop/Products.js
+++ Advanced Redux/src/components/Shop/Products.js
@@ -1,19 +1,40 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
-const Products = (props) => {
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 4,
+    title: 'My first book',
+    description:'My first book whas amazing!!',
+  },
+  {
+    id: 'p2',
+    price: 14,
+    title: 'My second book',
+    description:'My second book whas amazing!!',
+  },
+  {
+    id: 'p3',
+    price: 7,
+    title: 'My third book',
+    description:'My third book is not so good!!',
+  }
+];
+
+function Products(props) {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (<ProductItem
+          key={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description} />))}
       </ul>
     </section>
   );
-};
+}
 
 export default Products;
