c0170756605b0bee214727c8f80f4d61c95a2152
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-21 20:35:15 +0200

    Update

--- Practice project3/src/components/quotes/QuoteList.js
+++ Practice project3/src/components/quotes/QuoteList.js
@@ -1,13 +1,19 @@
 import { Fragment } from 'react';
-
+import { useHistory, useLocation } from 'react-router-dom';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+  console.log(location);
+  const sortHandler = () => {
+    history.push('/quotes?sort=asc');
+  };
   return (
     <Fragment>
       <div className={classes.sorting}>
-
+        <button onClick={sortHandler}>Sort Ascrnding</button>
       </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
