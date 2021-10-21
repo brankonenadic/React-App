9137a2c5615fe171e8f20a6062772e27c7c95e86
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-21 21:02:29 +0200

    Update

--- Practice project3/src/components/quotes/QuoteList.js
+++ Practice project3/src/components/quotes/QuoteList.js
@@ -3,20 +3,32 @@ import { useHistory, useLocation } from 'react-router-dom';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
+  const isSorting = queryParams.get('sort') === 'asc'; 
+  const sortingQuotes = sortQuotes(props.quotes, isSorting);
   const sortHandler = () => {
-    history.push('/quotes?sort=asc');
+    history.push('/quotes?sort=' + (isSorting ? 'desc' : 'asc'));
   };
   return (
     <Fragment>
       <div className={classes.sorting}>
-        <button onClick={sortHandler}>Sort Ascrnding</button>
+        <button onClick={sortHandler}>Sort {isSorting ? 'Descending' : 'Ascrnding'} </button>
       </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortingQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
