b2c88782ce1e36c2b6b9854d6fa7a168ac1d4ad4
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-21 10:30:42 +0200

    Update

--- Practice project3/src/pages/QuoteDetail.js
+++ Practice project3/src/pages/QuoteDetail.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams, Route } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 
 const QuoteDetail = () => {
     const params = useParams();
@@ -8,7 +9,7 @@ const QuoteDetail = () => {
             <h1>Quote Detail Page</h1>
             <p>{params.quoteId}</p>
             <Route path={`/quotes/${params.quoteId}/comments`}>
-            <p>Comments</p>
+            <Comments />
             </Route>
         </div>
     )
