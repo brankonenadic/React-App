f25460d992488f219ca89a37f87c6ad071080de4
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-21 10:00:06 +0200

    Update

--- Practice project3/src/pages/QuoteDetail.js
+++ Practice project3/src/pages/QuoteDetail.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const QuoteDetail = () => {
+    const params = useParams();
     return (
         <div>
             <h1>Quote Detail Page</h1>
+            <p>{params.quoteId}</p>
         </div>
     )
 }
