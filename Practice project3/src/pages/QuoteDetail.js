279e4d6e554c9d967230dee6266609a33fa8a1e0
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-22 11:55:02 +0200

    Update

--- Practice project3/src/pages/QuoteDetail.js
+++ Practice project3/src/pages/QuoteDetail.js
@@ -2,6 +2,7 @@ import { Fragment, useEffect } from 'react';
 import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 import useHttp from '../hooks/use-http';
 import { getSingleQuote } from '../lib/api';
 
@@ -10,12 +11,20 @@ const QuoteDetail = () => {
 
     const match = useRouteMatch();
     const params = useParams();
+    const {quoteId} = params;
 
     const { sendRequest, status, data: loadedData, error } = useHttp(getSingleQuote, true);
 
     useEffect(() => {
-        sendRequest();
-    }, [sendRequest]);
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+    if (status === 'pending') {
+        return (<div className="centered"><LoadingSpinner /></div>);
+    }
+    if (error) {
+        return (<p className="centered focused">{error}</p>);
+    }
 
     if (!detail) {
         return <h2>No Quote find !!!</h2>
