818bea6469f5d2f35b71a30b7643be8bc8321b2a
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-24 17:20:16 +0200

    Update

--- Auth-React/src/components/Profile/ProfileForm.js
+++ Auth-React/src/components/Profile/ProfileForm.js
@@ -5,7 +5,7 @@ const ProfileForm = () => {
     <form className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' autoComplete="off" />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
