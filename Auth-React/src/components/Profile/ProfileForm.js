4f196eb0d9d9eb62f3dacb1a94f4cd9adf7979c7
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-25 22:20:11 +0200

    Update

--- Auth-React/src/components/Profile/ProfileForm.js
+++ Auth-React/src/components/Profile/ProfileForm.js
@@ -1,11 +1,18 @@
+import { useRef } from 'react';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const newPassword = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredNewPassword = newPassword.current.value;
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' autoComplete="off" />
+        <input type='password' id='new-password' autoComplete="off" ref={newPassword} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
