67a72de2bac917709d1f8116e6c361643496662a
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-25 15:02:59 +0200

    Update

--- Auth-React/src/store/auth-context.js
+++ Auth-React/src/store/auth-context.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const authContext = React.createContext({
     token: '',
     isLogIn: false,
-    login: (token) => {},
-    logout: () => {}
+    login: (token) => { },
+    logout: () => { }
 });
+
+const AuthContextProvider = (props) => {
+    const [token, setToken] = useState(null);
+    const userIsLogIn = !!token;
+    const logInHandler = () => {
+        setToken(token);
+    };
+    const logOutHandler = () => {
+        settoken(null);
+    };
+    return
+    <authContext.Provider>{props.children}</authContext.Provider>
+};
