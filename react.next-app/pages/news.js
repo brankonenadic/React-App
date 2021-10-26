747a828b5116c0b84462f8aedfad416828d95d12
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-26 17:06:48 +0200

    Update

--- react.next-app/pages/news.js
+++ react.next-app/pages/news.js
@@ -1,7 +1,10 @@
 const NewsPage = () => {
 
     return (
-        <h1>This is the News Page</h1>
+        <div>
+            <h1>This is the News Page</h1>
+            <h3>Some Subtitle</h3>
+        </div>
     );
 
 };
