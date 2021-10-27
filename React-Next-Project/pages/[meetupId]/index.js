ec79bcaadfba8dee3c259651f67a2487c6c62b9b
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-27 13:18:51 +0200

    Update

--- React-Next-Project/pages/[meetupId]/index.js
+++ React-Next-Project/pages/[meetupId]/index.js
@@ -3,7 +3,10 @@ import { Fragment } from "react";
 const MeetupDetailPage = () => {
     return (
         <Fragment>
-
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg' alt='some meetup'/>
+            <h1>Some meetup</h1>
+            <address>Some address  , SomeCity 323892</address>
+            <p>Some description...</p>
         </Fragment>
     );
 };
