2124838d5b0686ca6833a84d1600fdd987cf6431
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-27 12:02:19 +0200

    Update

--- React-Next-Project/pages/index.js
+++ React-Next-Project/pages/index.js
@@ -1,1 +1,35 @@
-const 
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_LIST = [
+    {
+        id: 'm1',
+        title: 'First meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/1_riomaggiore_2012.jpg/1024px-1_riomaggiore_2012.jpg',
+        address: 'Some address, some city 71320',
+        description: 'My first meetup'
+    },
+    {
+        id: 'm2',
+        title: 'Second meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Corniglia_panorama.jpg',
+        address: 'Some address2, some city2 71320',
+        description: 'My second meetup'
+    },
+    {
+        id: 'm3',
+        title: 'Threed meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg',
+        address: 'Some address3, some city3 71320',
+        description: 'My three meetup'
+    },
+];
+
+const HomePage = () => {
+
+return (
+<MeetupList meetups={DUMMY_LIST}/>
+);
+
+};
+
+export default HomePage;
---
+++ React-Next-Project/pages/new-meetup/index.js
@@ -0,0 +1,13 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+
+const NewMeetup = () => {
+    const onAddHandler = () => {
+
+    };
+return (
+    <NewMeetupForm onAddMeetup={onAddHandler} />
+);
+};
+
+export default NewMeetup;
