e98f383dbf47f5a4408620e67ec9299a9d81de55
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-27 14:23:14 +0200

    Update

--- React-Next-Project/pages/[meetupId]/index.js
+++ React-Next-Project/pages/[meetupId]/index.js
@@ -1,13 +1,11 @@
-import { Fragment } from "react";
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetailPage = () => {
     return (
-        <Fragment>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg' alt='some meetup'/>
-            <h1>Some meetup</h1>
-            <address>Some address  , SomeCity 323892</address>
-            <p>Some description...</p>
-        </Fragment>
+        <MeetupDetail image='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg'
+        title='Some meetup'
+        address='Some address  , SomeCity 323892' description='Some description...' />
     );
 };
 
