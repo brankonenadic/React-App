e8ab9ac58d2d28972f67f43ed736f6271be5c2a7
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-28 17:52:13 +0200

    Update

--- React-Next-Project/pages/[meetupId]/index.js
+++ React-Next-Project/pages/[meetupId]/index.js
@@ -2,11 +2,39 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetailPage = () => {
+
     return (
         <MeetupDetail image='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg'
         title='Some meetup'
         address='Some address  , SomeCity 323892' description='Some description...' />
     );
+};
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            }
+        ]
+    }
+};
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+    return {
+        props: {
+         meetupData: {
+             image:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg',
+            id: meetupId,
+            title:'Some meetup',
+            address:'Some address  , SomeCity 323892',
+            description: 'Some description...'
+        }
+        }
+    };
 };
 
 export default MeetupDetailPage;
