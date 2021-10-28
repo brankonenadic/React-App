72b13bbecfa9137d0de7786cf0def46a0b9a8245
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-28 21:00:02 +0200

    Update

--- React-Next-Project/pages/[meetupId]/index.js
+++ React-Next-Project/pages/[meetupId]/index.js
@@ -5,8 +5,8 @@ const MeetupDetailPage = () => {
 
     return (
         <MeetupDetail image='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg'
-        title='Some meetup'
-        address='Some address  , SomeCity 323892' description='Some description...' />
+            title='Some meetup'
+            address='Some address  , SomeCity 323892' description='Some description...' />
     );
 };
 export async function getStaticPaths() {
@@ -14,27 +14,35 @@ export async function getStaticPaths() {
     const db = client.db();
 
     const meetupCollection = db.collection('meetup');
-    
-    const meetups = await meetupCollection.find({}, {_id: 1}).toArray();
+
+    const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
     client.close();
 
     return {
         fallback: false,
-        paths: meetups.map(meetup => ({params: {meetupId: meetup._id.toString()}}))
+        paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() } }))
     }
 };
 
 export async function getStaticProps(context) {
     const meetupId = context.params.meetupId;
+
+    const client = await MongoClient.connect('mongodb+srv://LaninTata:2810Lana15!@cluster0.t69mt.mongodb.net/meetups?retryWrites=true&w=majority');
+    const db = client.db();
+
+    const meetupCollection = db.collection('meetup');
+
+    const selectedMeetup = await meetupCollection.findOne({ _id: meetupId });
+    client.close();
     return {
         props: {
-         meetupData: {
-             image:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg',
-            id: meetupId,
-            title:'Some meetup',
-            address:'Some address  , SomeCity 323892',
-            description: 'Some description...'
-        }
+            meetupData: {
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg/1024px-Vernazza_and_the_sea%2C_Cinque_Terre%2C_Italy.jpg',
+                id: meetupId,
+                title: 'Some meetup',
+                address: 'Some address  , SomeCity 323892',
+                description: 'Some description...'
+            }
         }
     };
 };
