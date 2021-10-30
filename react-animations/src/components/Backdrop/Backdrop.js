a7bdefbd22986d00c047132f99cea2eccad337b6
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-10-30 11:34:26 +0200

    Update

--- react-animations/src/components/Backdrop/Backdrop.js
+++ react-animations/src/components/Backdrop/Backdrop.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+    const cssClasses = ['Backdrop' , props.show ? 'BackdropOpen' : 'BackdropClose' ];
+
+    return (
+        <div className="Backdrop"></div>
+    );
+};
 
 export default backdrop;
